305737d4b399423df9448009d79cf4fdf8deb811
Zondine <manuel.mesa2000@gmail.com>
2024-08-14 17:50:47 -0400

    Estaba doblemente comentado

--- GSR_Kine_USER_GUI.py
+++ GSR_Kine_USER_GUI.py
@@ -12,36 +12,6 @@ Manuel Mesa
 
 import tkinter as tk
 
-# Agregar un deslizador con los puertos en el cual se va a conectar el dispositivo al PC
-# 
-# Boton que para comprobar conexión
-#
-# Agregar algún icono que indique el estado del Arduino conectado/desconectado
-
-# Mostrar grafico y valores mientras se esta midiendo
-# Sacar los valores de arduino (??)
-# USAR serial graph del mismo arduino ???
-# Boton para inicio/paro de graficar en tiempo real
-
-# Guardar en CSV
-# abre comienze a guardar
-# cierre, guarda archivo con los valores, con fecha y hora de la muestra de los datos
-# Boton desde cuando hasta cuando grabar datos (Puede ser Grabar: inicio/stop)
-
-""""
-La idea de este código es ser la interfaz de usuario
--Ser GUI idealmente
--Tiene que mostrar un grafico en tiempo real de lo  que esta midiendo
--Queremos enteregue un .CSV (¿?) para leer en excel
-
-Manuel Mesa 
-13/08/24
-"""""
-# Librerias
-# Recordar agregarlas para los requirements.txt déspues, con sus respectivas versiones
-
-import tkinter as tk
-
 #Clase app para arduino
 
 class ArduinoGUI:
@@ -71,6 +41,8 @@ class ArduinoGUI:
 
 #Funciones
 
+
+#main loop
 
 if __name__ == "__main__":
     root = tk.Tk()
