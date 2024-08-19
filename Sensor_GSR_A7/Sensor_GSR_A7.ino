698a52395e8a3edb7acce53ac4729bea3f2f5a4b
Zondine <manuel.mesa2000@gmail.com>
2024-08-19 18:21:50 -0400

    Falta considerar los prints, continuar

--- Sensor_GSR_A7/Sensor_GSR_A7.ino
+++ Sensor_GSR_A7/Sensor_GSR_A7.ino
@@ -1,8 +1,13 @@
 const int GSR=A7;
 int sensorValue=0;
-int gsr_average=0;
-int Serial_speed=115200; 
-int promedio_variable=10;
+
+unsigned long lastTime = 0;
+unsigned long currentTime = 0;
+float samplingFrequency = 0;
+
+int Serial_speed=9600; 
+
+//simplificación del código para comprobar la frecuencia de muestreo
 
 #include "utilidades.h"
 void setup(){
@@ -10,13 +15,22 @@ void setup(){
 }
 
 void loop(){
-  long sum=0;
-  for(int i=0;i<promedio_variable;i++)           //Average the 10 measurements to remove the glitch
-      {
-      sensorValue=analogRead(GSR);
-      sum += sensorValue;
-      delay(5);
-      }
-   gsr_average = sum/10;
-   Serial.println(gsr_average);
+  currentTime = micros();  // Tiempo actual en microsegundos
+
+  sensorValue = analogRead(A7);
+  Serial.print("Valor sensor: ");
+  Serial.print(sensorValue);
+  Serial.println(" ");
+
+ 
+//Revisar las variables
+
+  unsigned long dt = currentTime - lastTime;  // Tiempo entre lecturas
+  samplingFrequency = 1000000 / dt;  // Frecuencia de muestreo en Hz
+
+  Serial.print("Frecuencia de muestreo: ");
+  Serial.print(samplingFrequency);
+  Serial.println("Hz");
+
+  lastTime = currentTime;  // Actualizar el tiempo para la siguiente iteración
 }
