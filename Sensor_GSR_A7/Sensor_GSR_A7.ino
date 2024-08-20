7bfdbe6652afb7bfaa01222e6d3b41357dc12b58
Zondine <manuel.mesa2000@gmail.com>
2024-08-20 17:16:13 -0400

    a la calma, simplemente moví los archivos. Es posible que sea una solución más óptima usar la libreria pyQT con pyQtgraph

--- Sensor_GSR_A7/Sensor_GSR_A7.ino
+++ Sensor_GSR_A7/Sensor_GSR_A7.ino
@@ -1,19 +1,15 @@
-const int GSR=A7;
-int sensorValue=0;
-int gsr_average=0;
+int analogPin = A7;     
+int data = 0;           
 
 void setup(){
-  Serial.begin(9600);
+
+  Serial.begin(9600);            //  setup serial
+
 }
 
 void loop(){
-  long sum=0;
-  for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
-      {
-      sensorValue=analogRead(GSR);
-      sum += sensorValue;
-      delay(5);
-      }
-   gsr_average = sum/10;
-   Serial.println(gsr_average);
-}
+  
+    data = analogRead(analogPin);    // read the input pin
+    Serial.println(data);   
+
+} // Void Loop
