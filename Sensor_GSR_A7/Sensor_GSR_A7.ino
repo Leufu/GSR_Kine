9c5c7447f4fdc2da63f3a135e3ca93052cf07d19
Zondine <manuel.mesa2000@gmail.com>
2024-08-23 16:38:18 -0400

    Codigo de detección con promedio y ajustada para muestrear a 10 Hz aproximadamente

--- Sensor_GSR_A7/Sensor_GSR_A7.ino
+++ Sensor_GSR_A7/Sensor_GSR_A7.ino
@@ -1,15 +1,26 @@
 int analogPin = A7;     
+int dataRaw=0;
 int data = 0;           
+long sum=0;
 
 void setup(){
 
   Serial.begin(9600);            //  setup serial
 
 }
-
 void loop(){
-  
-    data = analogRead(analogPin);    // read the input pin
-    Serial.println(data);   
-
+//unsigned long startTime = millis(); // Registra el tiempo de inicio
+    long sum = 0;
+    for(int i=0;i<=100;i++){
+     dataRaw = analogRead(analogPin);    // read the input pin 
+       sum += dataRaw;
+      delay(1);
+    }
+   data = sum/100;
+//   Serial.print("Data: ");
+   Serial.println(data);   
+//   unsigned long endTime = millis(); // Registra el tiempo de fin
+//   long elapsedTime = endTime - startTime; // Calcula el tiempo transcurrido
+//   Serial.print("Elapsed time (ms): ");
+//   Serial.println(elapsedTime);
 } // Void Loop
