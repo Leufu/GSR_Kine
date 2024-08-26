e8a6384a759c6bf7d051bfe69f654811d08f3e70
Zondine <manuel.mesa2000@gmail.com>
2024-08-26 12:10:06 -0400

    cambio en el main

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from GUI import ArduinoGUI
-from ArduinoSerialComm import SerialComm
-from gestion_data import DataHandler
+from gui import SerialMonitorApp
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-
-    serial_comm = SerialComm()
-    data_handler = DataHandler()
-
-    gui = ArduinoGUI(serial_comm, data_handler)
-    gui.show()
-
+    window = SerialMonitorApp()
+    window.show()
     sys.exit(app.exec_())
