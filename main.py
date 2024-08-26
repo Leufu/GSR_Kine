23c52e4bdb61223f11b960fb1b0fec298ed1267b
Zondine <manuel.mesa2000@gmail.com>
2024-08-26 12:21:35 -0400

    pequeño bug

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from gui import SerialMonitorApp
+from GUI import SerialMonitorApp
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
