862cc7a9dbb48a67be32bb9cc4158612e347968c
Zondine <manuel.mesa2000@gmail.com>
2024-08-26 12:11:36 -0400

    tiene lineas de depuracion

---
+++ serial_communication.py
@@ -0,0 +1,46 @@
+import serial
+import serial.tools.list_ports
+
+class SerialCommunication:
+    def __init__(self):
+        self.ser = None
+        self.port = None
+        self.baudrate = 9600
+
+    def get_available_ports(self):
+        port_devices = []
+        for port in serial.tools.list_ports.comports():
+            if "USB-SERIAL" in port.description:
+                port_devices.append(port.device)
+        print(f"Available ports: {port_devices}")  # Depuración
+        return port_devices
+
+    def connect(self, port):
+        try:
+            print(f"Trying to connect to {port}...")  # Depuración
+            self.ser = serial.Serial(port, self.baudrate, timeout=1)
+            print(f"Connected to {port}")  # Depuración
+            return True
+        except serial.SerialException as e:
+            print(f"Failed to connect: {e}")  # Depuración
+            return False
+
+    def disconnect(self):
+        if self.ser:
+            print(f"Disconnecting from {self.ser.port}")  # Depuración
+            self.ser.close()
+            self.ser = None
+
+    def read_data(self):
+        if self.ser:
+#            print("Starting to read data...")  # Depuración
+            while self.ser.is_open:
+                try:
+                    line = self.ser.readline().decode('utf-8').strip()
+#                    print(f"Read line: {line}")  # Depuración
+                    if line.isnumeric():
+                        return float(line)
+                except serial.SerialException as e:
+                    print(f"Serial exception: {e}")  # Depuración
+                    break
+            print("Stopped reading data.")  # Depuración
