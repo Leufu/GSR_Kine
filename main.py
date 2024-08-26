44745a0136920fbfec374b3070433ad71c2fe30c
Zondine <manuel.mesa2000@gmail.com>
2024-08-26 12:35:06 -0400

    comentado

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from GUI import SerialMonitorApp
 
+#main loop
+
+"""
+Inicio fácil
+
+la idea general de como debe funcionar este programa es:
+-boton para conectar al arduino
+-boton para graficar
+-boton para deconectar
+-gráfica en tiempo real
+-guardado en .csv
+
+"""
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = SerialMonitorApp()
